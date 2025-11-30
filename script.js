23eef971c222a0ac594870910f3674b98f556357
ecraw4d <116666131+ecraw4d@users.noreply.github.com>
2025-11-29 20:28:04 -0600

    Initialize Leaflet map with markers and shapes

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+// Initialize the map
+var map = L.map('map').setView([51.505, -0.09], 13);
 
+// Add OpenStreetMap tiles
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Add a marker
+var marker = L.marker([51.5, -0.09]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+// Add a circle
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+circle.bindPopup("I am a circle.");
+
+// Add a polygon
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+polygon.bindPopup("I am a polygon.");
+
+// Click event for the map
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
