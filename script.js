b4cb792bae1a0b36efce391789635c5b818dcda8
ecraw4d <116666131+ecraw4d@users.noreply.github.com>
2025-12-13 15:49:43 -0600

    Add population density info control to map

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    var contents = props ? `<b>${props.name}</b><br />${props.density} people / mi<sup>2</sup>` : 'Hover over a state';
+    this._div.innerHTML = `<h4>US Population Density</h4>${contents}`;
+};
+
+info.addTo(map);
 
 function getColor(d) {
     return d > 1000 ? '#800026' :
-           d > 500  ? '#BD0026' :
-           d > 200  ? '#E31A1C' :
-           d > 100  ? '#FC4E2A' :
-           d > 50   ? '#FD8D3C' :
-           d > 20   ? '#FEB24C' :
-           d > 10   ? '#FED976' :
-                      '#FFEDA0';
+        d > 500  ? '#BD0026' :
+        d > 200  ? '#E31A1C' :
+        d > 100  ? '#FC4E2A' :
+        d > 50   ? '#FD8D3C' :
+        d > 20   ? '#FEB24C' :
+        d > 10   ? '#FED976' : '#FFEDA0';
 }
 
 function style(feature) {
@@ -24,13 +36,13 @@ function style(feature) {
         opacity: 1,
         color: 'white',
         dashArray: '3',
-        fillOpacity: 0.7
-        fillColor: getColor(feature.properties.desnity)
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
     };
 }
 
 function highlightFeature(e) {
-    const layer = e.target;
+    var layer = e.target;
 
     layer.setStyle({
         weight: 5,
@@ -43,49 +55,49 @@ function highlightFeature(e) {
 
     info.update(layer.feature.properties);
 }
-const geojson = L.geoJson(statesData, {
-		style,
-		onEachFeature
-	}).addTo(map);
 
-	function resetHighlight(e) {
-		geojson.resetStyle(e.target);
-		info.update();
-	}
+var geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
 
-	function zoomToFeature(e) {
-		map.fitBounds(e.target.getBounds());
-	}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
 
-	function onEachFeature(feature, layer) {
-		layer.on({
-			mouseover: highlightFeature,
-			mouseout: resetHighlight,
-			click: zoomToFeature
-		});
-	}
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
 
-	map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
 
+map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
 
-	const legend = L.control({position: 'bottomright'});
+var legend = L.control({position: 'bottomright'});
 
-	legend.onAdd = function (map) {
+legend.onAdd = function (map) {
 
-		const div = L.DomUtil.create('div', 'info legend');
-		const grades = [0, 10, 20, 50, 100, 200, 500, 1000];
-		const labels = [];
-		let from, to;
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades = [0, 10, 20, 50, 100, 200, 500, 1000];
+    var labels = [];
+    var from, to;
 
-		for (let i = 0; i < grades.length; i++) {
-			from = grades[i];
-			to = grades[i + 1];
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
 
-			labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
-		}
+        labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+    }
 
-		div.innerHTML = labels.join('<br>');
-		return div;
-	};
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
 
-	legend.addTo(map);
+legend.addTo(map);
