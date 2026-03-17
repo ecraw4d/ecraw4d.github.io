26e90c69b3f8ec8ec4ee5c2bd34b3e61c9e26657
ecraw4d <ethancc5@illinois.edu>
2026-03-17 14:56:46 -0500

    update maps.html

---
+++ project1.js
@@ -0,0 +1,103 @@
+// Initialize the map and target the "project-map" div
+var map = L.map('project-map').setView([37.8, -96], 4);
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Custom info control
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    var contents = props ? `<b>${props.name}</b><br />${props.density} people / mi<sup>2</sup>` : 'Hover over a state';
+    this._div.innerHTML = `<h4>US Population Density</h4>${contents}`;
+};
+
+info.addTo(map);
+
+// Color scale
+function getColor(d) {
+    return d > 1000 ? '#7a0177' :
+        d > 500  ? '#ae017e' :
+        d > 200  ? '#dd3497' :
+        d > 100  ? '#f768a1' :
+        d > 50   ? '#fa9fb5' :
+        d > 20   ? '#fcc5c0' :
+        d > 10   ? '#fde0dd' : '#fff7f3';
+}
+
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: getColor(feature.properties.density)
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+    layer.bringToFront();
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+// Ensure statesData is loaded before this runs!
+var geojson = L.geoJson(statesData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
+
+// Legend
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    var grades =[0, 10, 20, 50, 100, 200, 500, 1000];
+    var labels =[];
+    var from, to;
+
+    for (var i = 0; i < grades.length; i++) {
+        from = grades[i];
+        to = grades[i + 1];
+        labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+    }
+
+    div.innerHTML = labels.join('<br>');
+    return div;
+};
+
+legend.addTo(map);
--- script.js
+++ script.js
@@ -1,103 +1,27 @@
-var map2 = L.map('map2').setView([37.8, -96], 4);
+// Wait until the webpage is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    
+    // Select the button and the gallery container
+    const layoutToggleBtn = document.getElementById('layout-toggle');
+    const mapGallery = document.getElementById('map-gallery');
 
-var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map2);
-
-var info = L.control();
-
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info');
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-    var contents = props ? `<b>${props.name}</b><br />${props.density} people / mi<sup>2</sup>` : 'Hover over a state';
-    this._div.innerHTML = `<h4>US Population Density</h4>${contents}`;
-};
-
-info.addTo(map2);
-
-function getColor(d) {
-    return d > 1000 ? '#7a0177' :
-        d > 500  ? '#ae017e' :
-        d > 200  ? '#dd3497' :
-        d > 100  ? '#f768a1' :
-        d > 50   ? '#fa9fb5' :
-        d > 20   ? '#fcc5c0' :
-        d > 10   ? '#fde0dd' : '#fff7f3';
-}
-
-function style(feature) {
-    return {
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: 0.7,
-        fillColor: getColor(feature.properties.density)
-    };
-}
-
-function highlightFeature(e) {
-    var layer = e.target;
-
-    layer.setStyle({
-        weight: 5,
-        color: '#666',
-        dashArray: '',
-        fillOpacity: 0.7
-    });
-
-    layer.bringToFront();
-
-    info.update(layer.feature.properties);
-}
-
-var geojson = L.geoJson(statesData, {
-    style: style,
-    onEachFeature: onEachFeature
-}).addTo(map);
-
-function resetHighlight(e) {
-    geojson.resetStyle(e.target);
-    info.update();
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-    layer.on({
-        mouseover: highlightFeature,
-        mouseout: resetHighlight,
-        click: zoomToFeature
-    });
-}
-
-map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
-
-var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend');
-    var grades = [0, 10, 20, 50, 100, 200, 500, 1000];
-    var labels = [];
-    var from, to;
-
-    for (var i = 0; i < grades.length; i++) {
-        from = grades[i];
-        to = grades[i + 1];
-
-        labels.push(`<i style="background:${getColor(from + 1)}"></i> ${from}${to ? `&ndash;${to}` : '+'}`);
+    // Check if we are actually on the maps page (so this doesn't error out on index.html)
+    if (layoutToggleBtn && mapGallery) {
+        
+        layoutToggleBtn.addEventListener('click', () => {
+            // Check current layout
+            if (mapGallery.classList.contains('gallery-grid')) {
+                // Switch to List View
+                mapGallery.classList.remove('gallery-grid');
+                mapGallery.classList.add('gallery-list');
+                layoutToggleBtn.textContent = 'Switch to Grid View';
+            } else {
+                // Switch to Grid View
+                mapGallery.classList.remove('gallery-list');
+                mapGallery.classList.add('gallery-grid');
+                layoutToggleBtn.textContent = 'Switch to List View';
+            }
+        });
     }
-
-    div.innerHTML = labels.join('<br>');
-    return div;
-};
-
-legend.addTo(map);
+    
+});
